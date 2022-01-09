b1a1c4b25101e7506948356c1bbb6f7e83ca8acb
Alex McFarland <mcfarland.alex@gmail.com>
2022-01-08 21:13:46 -0500

    update script

--- hack/fix-protheus-stack-ugprade-webhook-error.sh
+++ hack/fix-protheus-stack-ugprade-webhook-error.sh
@@ -1,3 +1,4 @@
+# https://github.com/prometheus-community/helm-charts/issues/108#issuecomment-825689328
 # change version (up or down) then:
 kubectl delete mutatingwebhookconfiguration/kube-prometheus-stack-admission -A
 kubectl delete validatingwebhookconfiguration/kube-prometheus-stack-admission -A
