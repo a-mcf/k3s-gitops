666506d370a43588dacb3ffd8a3415429c864147
Alex McFarland <mcfarland.alex@gmail.com>
2022-01-08 21:12:00 -0500

    hack: add script to work around helm upgrade issues.

--- hack/fix-protheus-stack-ugprade-webhook-error.sh
+++ hack/fix-protheus-stack-ugprade-webhook-error.sh
@@ -1,2 +1,3 @@
+# change version (up or down) then:
 kubectl delete mutatingwebhookconfiguration/kube-prometheus-stack-admission -A
 kubectl delete validatingwebhookconfiguration/kube-prometheus-stack-admission -A
