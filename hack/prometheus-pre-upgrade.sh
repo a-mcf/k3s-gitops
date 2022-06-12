008ce22285657d5ab4464557635f36c213201161
Alex McFarland <mcfarland.alex@gmail.com>
2022-06-12 12:01:09 -0400

    pre-commit fixes

--- hack/prometheus-pre-upgrade.sh
+++ hack/prometheus-pre-upgrade.sh
@@ -2,3 +2,5 @@
 # change version (up or down) then:
 kubectl delete mutatingwebhookconfiguration/kube-prometheus-stack-admission -A
 kubectl delete validatingwebhookconfiguration/kube-prometheus-stack-admission -A
+
+echo "REMEMBER TO UPDATE ASSOCIATED CRDS!"
